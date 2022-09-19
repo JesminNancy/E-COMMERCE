4757028850a5c10cb473e0a3fd98eb118c164ad0
JesminNancy <jesminnancy@hotmail.com>
2022-09-19 20:49:56 +0600

    How to make logout button in E-commerce

--- eshop/resources/views/layouts/app.blade.php
+++ eshop/resources/views/layouts/app.blade.php
@@ -63,7 +63,7 @@
                                     </a>
                                 </li>
                                 <li>
-                                <a class="dropdown-item" href="{{ route('logout') }}        "            onclick="event.preventDefault();
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                  document.getElementById('logout-form').
                                  submit();">
                                      {{ __('Logout') }}
