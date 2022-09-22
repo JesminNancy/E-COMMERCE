e11dd50c35963ad35777b93463bc2648f34a454e
JesminNancy <jesminnancy@hotmail.com>
2022-09-22 22:43:55 +0600

    How to make navbar and slider for ecommerce  Frontend layout setup

---
+++ eshop/resources/views/frontend/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.front')
+
+@section('title')
+    Welcome to E-Shop
+@endsection
+@section('content')
+@include('layouts.inc.slider')
+   <h1>welcome</h1>
+
+@endsection
--- eshop/routes/web.php
+++ eshop/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\FrontendController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Frontend\FrontController;
 
 
 /*
@@ -17,10 +18,10 @@ use App\Http\Controllers\Admin\ProductController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+ Route::get('/',[FrontController::class, 'index']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
